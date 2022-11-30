6c97c85031a9cb2f8312ab47ceea2001c1ebc3f6
Lukasz Jozwiak <lukasz.jozwiak@gft.com>
2022-11-30 08:43:31 +0100

    remove unused

--- pd/outputs.tf
+++ pd/outputs.tf
@@ -1,8 +1,3 @@
-output "demo_service_cloudwatch_high_integration_url" {
-  value = "https://events.eu.pagerduty.com/integration/${pagerduty_service_integration.demo_service_cloudwatch.integration_key}/enqueue"
-}
-
-
 output "demo_service_events_integration_endpoint" {
   value = "https://events.pagerduty.com/x-ere/${pagerduty_event_orchestration.demo_service_orchestration.integration[0].parameters[0].routing_key}"
 }
