207856890fb7b05c9e2615f3a69b15b1c6e5b29b
Lukasz Jozwiak <lukasz.jozwiak@gft.com>
2022-11-30 13:37:49 +0100

    Add ecs alert

--- aws/main.tf
+++ aws/main.tf
@@ -44,17 +44,21 @@ resource "aws_sns_topic_subscription" "demo_service_qa_events_subscription" {
   endpoint  = var.demo_service_qa_events_integration_endpoint
 }
 
-resource "aws_cloudwatch_metric_alarm" "demo_service_qa_warning" {
-  alarm_name          = "demo-service-qa-FCP_LOW_LEVEL_ALERT"
+resource "aws_cloudwatch_metric_alarm" "maximum_service_capacity_utilized" {
+  alarm_name          = "demo-service-qa-maximum-capacity-FCP_LOW_LEVEL_ALERT"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
+  evaluation_periods  = "8"
+  metric_name         = "RunningTaskCount"
+  namespace           = "ECS/ContainerInsights"
+  period              = "900"
   statistic           = "Average"
-  threshold           = "80"
-  alarm_description   = "[Low] This metric monitors ec2 cpu utilization"
+  threshold           = "4"
+  alarm_description   = "Service scaled to maximum cpacity for over 2 hours"
   alarm_actions       = [aws_sns_topic.demo_service_qa_events.arn]
+  dimensions = {
+    ClusterName = "demo-cluster"
+    ServiceName = "demo-service-qa"
+  }
 }
 
 resource "aws_cloudwatch_metric_alarm" "demo_service_qa_error" {
